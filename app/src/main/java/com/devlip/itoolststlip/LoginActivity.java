7dd5d02d57f8f135e4d166e8168ed1dc7cbda559
DevLipatov <yaroslav.lipatov.90@gmail.com>
2017-01-21 18:36:09 +0300

    start and sdk integrate

---
+++ app/src/main/java/com/devlip/itoolststlip/LoginActivity.java
@@ -0,0 +1,7 @@
+package com.devlip.itoolststlip;
+
+import android.support.v7.app.AppCompatActivity;
+import com.facebook.FacebookSdk;
+
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/devlip/itoolststlip/MainActivity.java
@@ -0,0 +1,43 @@
+package com.devlip.itoolststlip;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    //new??
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+}
